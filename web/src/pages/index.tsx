ccc7a1efb84a0d78eb2152830852c19c3fed78d6
Marcelo Bueno <marcelorezendebueno@gmail.com>
2022-04-29 14:49:04 -0300

    Aula 04 - Integrando front-end e back-end

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,24 +1,26 @@
-import { getAccessToken, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { getSession } from "@auth0/nextjs-auth0";
+import { GetServerSideProps } from "next";
 
 export default function Home() {
-  const { user } = useUser();
-
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <pre>
-        {JSON.stringify(user, null, 2)}
-      </pre>
-    </div>
-  );
+  return null;
 }
 
-export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: async ({ req, res }) => {
-    console.log(getAccessToken(req, res));
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
 
+  if (!session) {
     return {
-      props: {}
-    }
+      redirect: {
+        destination: "/api/auth/login",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      redirect: {
+        destination: "/app",
+        permanent: false,
+      },
+    };
   }
-});
+};
